fe901ccd0597e6c3d2306ab2e6e47530c62a3ee7
rmnaser90 <rmnaser90@gmail.com>
2020-10-23 09:58:09 +0300

    excercise 1 done

---
+++ conditional-statements/main.js
@@ -0,0 +1,12 @@
+// Exercise 1
+var numChild= prompt("enter the number of your children")
+var isCareful=false;
+
+if (isCareful) {
+
+    numChild++;
+    
+}
+console.log(numChild);
+// Exercise 2
+
