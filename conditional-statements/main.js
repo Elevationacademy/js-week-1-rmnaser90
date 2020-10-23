9ad7fc89c51728760780f618a76a9c221a531cfd
rmnaser90 <rmnaser90@gmail.com>
2020-10-23 10:03:58 +0300

    excersice 1 commented excerces2 is done

--- conditional-statements/main.js
+++ conditional-statements/main.js
@@ -1,12 +1,22 @@
 // Exercise 1
-var numChild= prompt("enter the number of your children")
-var isCareful=false;
+// var numChild= prompt("enter the number of your children")
+// var isCareful=false;
 
-if (isCareful) {
+// if (isCareful) {
 
-    numChild++;
+//     numChild++;
     
-}
-console.log(numChild);
+// }
+// console.log(numChild);
+
+
+
 // Exercise 2
 
+var silverwareCount=prompt("enter the number of silverware:");
+
+if (silverwareCount%2) {
+    console.log("something is missing");
+}else{
+console.log("all good");
+}
