352d9d9e303a844b9f56b750531827765a6a2f78
rmnaser90 <rmnaser90@gmail.com>
2020-10-24 16:41:10 +0300

    excercis 2 commented Excercise 3 done

--- conditional-statements/main.js
+++ conditional-statements/main.js
@@ -1,4 +1,4 @@
-// Exercise 1
+// // Exercise 1
 // var numChild= prompt("enter the number of your children")
 // var isCareful=false;
 
@@ -11,12 +11,29 @@
 
 
 
-// Exercise 2
+// // Exercise 2
 
-var silverwareCount=prompt("enter the number of silverware:");
+// var silverwareCount=prompt("enter the number of silverware:");
 
-if (silverwareCount%2) {
-    console.log("something is missing");
+// if (silverwareCount%2) {
+//     console.log("something is missing");
+// }else{
+// console.log("all good");
+// }
+
+// Excerices 3
+
+let performance = "stellar"
+let salary = 10000
+const goodBonus = 1000
+const stellarBonus = 3000
+
+if (performance == "stellar"){
+
+
+    salary+=stellarBonus;
 }else{
-console.log("all good");
+
+    salary+=goodBonus;
 }
+console.log("Your new salary is:"+salary);
