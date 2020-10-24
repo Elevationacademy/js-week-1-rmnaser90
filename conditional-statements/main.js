de03f7a70e0f95ee287335caf4eee95cc0c415e3
rmnaser90 <rmnaser90@gmail.com>
2020-10-24 20:02:38 +0300

    comenting excercise 5

--- conditional-statements/main.js
+++ conditional-statements/main.js
@@ -52,23 +52,23 @@
 // }
 
 // // excercise 5
-const a = 3
-let b = 2
-let c = 0 || 12
-let d
-console.log(a+" "+b+" "+c+" "+d);
-b = c  //b=12
-b++    // b=13
-if(d){   // d undefined
-  b = a
-}
+// const a = 3
+// let b = 2
+// let c = 0 || 12
+// let d
+// console.log(a+" "+b+" "+c+" "+d);
+// b = c  //b=12
+// b++    // b=13
+// if(d){   // d undefined
+//   b = a
+// }
 
-d = a + (b * c)  //  3+ (13*12)=159
-d++  // d= 160
-b += 2; // b =15
+// d = a + (b * c)  //  3+ (13*12)=159
+// d++  // d= 160
+// b += 2; // b =15
 
-  // 3  15  12   160
-console.log(a+" "+b+" "+c+" "+d);
+//   // 3  15  12   160
+// console.log(a+" "+b+" "+c+" "+d);
 
 
 // // Excercise 6
