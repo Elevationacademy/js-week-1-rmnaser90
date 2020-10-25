d95f816b6173769c4f915df5118aec1665bd3b69
rmnaser90 <rmnaser90@gmail.com>
2020-10-25 23:08:53 +0300

    Excercise 1 done

---
+++ objects/main.js
@@ -0,0 +1,24 @@
+//Excercise 1
+let p1= {
+name: 'Robert',
+age: 30,
+city:'new york'
+}
+
+let p2= {
+
+name: 'Jill',
+age: 30,
+city: 'new york'
+}
+
+
+if(p1.age==p2.age){
+
+if (p1.city==p2.city) {
+    console.log("Jill wanted to date Robert");
+}else {
+    console.log("Jill wanted to date Robert, but couldn't");
+}
+
+}
