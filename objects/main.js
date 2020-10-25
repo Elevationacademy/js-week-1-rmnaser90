d32c303c6dedd7933b71697fc9d16f049e869296
rmnaser90 <rmnaser90@gmail.com>
2020-10-25 23:16:11 +0300

    Excercise 2 done

--- objects/main.js
+++ objects/main.js
@@ -1,24 +1,48 @@
-//Excercise 1
-let p1= {
-name: 'Robert',
-age: 30,
-city:'new york'
-}
+// //Excercise 1
+// let p1= {
+// name: 'Robert',
+// age: 30,
+// city:'new york'
+// }
 
-let p2= {
+// let p2= {
 
-name: 'Jill',
-age: 30,
-city: 'new york'
-}
+// name: 'Jill',
+// age: 30,
+// city: 'new york'
+// }
 
 
-if(p1.age==p2.age){
+// if(p1.age==p2.age){
 
-if (p1.city==p2.city) {
-    console.log("Jill wanted to date Robert");
-}else {
-    console.log("Jill wanted to date Robert, but couldn't");
-}
+// if (p1.city==p2.city) {
+//     console.log("Jill wanted to date Robert");
+// }else {
+//     console.log("Jill wanted to date Robert, but couldn't");
+// }
 
-}
+// }
+
+//Excecise 2
+
+let myList=[
+    {
+        name: 'Rami',
+        age: 30,
+        city:'Jerusalem'
+    },
+    {
+        name: 'Ahmad',
+        age: 28,
+        city:'Jerusalem'
+
+    }];
+    console.log(myList[0].age);
+
+    myList[0].age=32;
+
+console.log(myList[0].age);
+
+myList.splice(1,1);
+
+console.log(myList);
