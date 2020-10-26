0823b9d7db1876df115ac0dbac15347b55c835de
rmnaser90 <rmnaser90@gmail.com>
2020-10-26 12:23:33 +0300

    exercise 1 done

---
+++ loops/main.js
@@ -0,0 +1,10 @@
+
+// Excercise 1
+let names= ['Rami', 'Ahmad','Zein'];
+let ages= [30,28,19];
+
+
+for (let i = 0; i < names.length; i++) {
+    console.log(`${names[i]} is ${ages[i]} years old.`);
+    
+}
