d38caaa508de04b3c1487002cb5b3c92a8b22b97
rmnaser90 <rmnaser90@gmail.com>
2020-10-26 12:27:03 +0300

    Exercise 2 done

--- loops/main.js
+++ loops/main.js
@@ -1,10 +1,19 @@
 
-// Excercise 1
-let names= ['Rami', 'Ahmad','Zein'];
-let ages= [30,28,19];
+// // Exercise 1
+// let names= ['Rami', 'Ahmad','Zein'];
+// let ages= [30,28,19];
 
 
-for (let i = 0; i < names.length; i++) {
-    console.log(`${names[i]} is ${ages[i]} years old.`);
+// for (let i = 0; i < names.length; i++) {
+//     console.log(`${names[i]} is ${ages[i]} years old.`);
     
+// }
+
+// Exercise 2
+let sum =0;
+let myArray=[1,4,2,65,34,21,8,9];
+
+for (const i in myArray) {
+    sum+=myArray[i];
 }
+console.log(sum);
