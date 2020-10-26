1e8f4957dc848a2f8f0220a9cc25b609ec64f252
rmnaser90 <rmnaser90@gmail.com>
2020-10-26 13:36:17 +0300

    Exercise 8 done

--- loops/main.js
+++ loops/main.js
@@ -55,7 +55,7 @@
 //     }
 // }
 
-// Exercise 7
+// Exercise 7 & 8
 
 const names = ["Ashley", "Donovan", "Lucas"]
 const ages = [23, 47, 18]
@@ -68,3 +68,7 @@ for (let i = 0; i < names.length; i++) {
 }
 
 console.log(people);
+
+for (let person of people) {
+    console.log(person.name+" is "+person.age+" years old.");
+}
