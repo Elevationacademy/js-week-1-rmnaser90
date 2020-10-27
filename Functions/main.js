e399997af93c6070b318f14aca55597751e6060e
rmnaser90 <rmnaser90@gmail.com>
2020-10-27 12:42:54 +0300

    exercise 5 done

--- Functions/main.js
+++ Functions/main.js
@@ -31,34 +31,54 @@
 //     console.log(age);
     
 
-// Exercise 3
+// // Exercise 3
 
-const isEven= function(num){
+// const isEven = function(num){
 
-if (num%2) {
-    return false
-}else{
-    return true
-}
+// if (num%2) {
+//     return false
+// }else{
+//     return true
+// }
 
-}
-randomNum = Math.floor(Math.random()*100);
-console.log(randomNum);
-console.log(isEven(randomNum));
+// }
+// randomNum = Math.floor(Math.random()*100);
+// console.log(randomNum);
+// console.log(isEven(randomNum));
 
 
-console.log("Exercise 4 below");
-// Exercise 4
+// console.log("Exercise 4 below");
+// // Exercise 4
 
-const isEvenInArray = function (arr) {
+// const isEvenInArray = function (arr) {
     
-    for (const num of arr) {
-        if (!isEven(num)) {
-            console.log(num);
+//     for (const num of arr) {
+//         if (!isEven(num)) {
+//             console.log(num);
             
+//         }
+//     }
+// }
+
+// let numArr=[3,4,5,8,10,11,6,22,23,71]
+// isEvenInArray(numArr)
+
+// Exercise 5
+
+
+const checkExists=function (arr,num) {
+
+    for (const n of arr) {
+        if (n==num) {
+            return true;
+        }else {
+            return false;
         }
     }
+    
 }
 
-let numArr=[3,4,5,8,10,11,6,22,23,71]
-isEvenInArray(numArr)
+const needle= 7;
+const stack=[3,4,2,5,1,6,2];
+
+console.log( checkExists(stack,needle) );
