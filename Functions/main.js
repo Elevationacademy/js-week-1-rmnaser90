5fc6e3e37d3c54d70104f319a9ea3979a3b08136
rmnaser90 <rmnaser90@gmail.com>
2020-10-27 11:51:34 +0300

    Exercise 3 done

--- Functions/main.js
+++ Functions/main.js
@@ -14,19 +14,34 @@
 // console.log(age);
 
 
-// exercise 2
+// // exercise 2
 
 
 
-const calcAge = function(currentYear,birthYear){
-    let age = currentYear-birthYear;
-    let result = `You are either ${age} or ${age-1}`
-    return result;
+// const calcAge = function(currentYear,birthYear){
+//     let age = currentYear-birthYear;
+//     let result = `You are either ${age} or ${age-1}`
+//     return result;
     
     
     
-    }
+//     }
     
-    const age = calcAge(2018, 2015);
-    console.log(age);
+//     const age = calcAge(2018, 2015);
+//     console.log(age);
     
+
+// Exercise 3
+
+const isEven= function(num){
+
+if (num%2) {
+    return false
+}else{
+    return true
+}
+
+}
+randomNum = Math.floor(Math.random()*100);
+console.log(randomNum);
+console.log(isEven(randomNum));
