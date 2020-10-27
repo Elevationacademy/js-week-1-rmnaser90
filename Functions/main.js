b965cf1ca5d58573bc9099db49f1c17cd4a2a761
rmnaser90 <rmnaser90@gmail.com>
2020-10-27 11:42:12 +0300

    exercise 2 done

--- Functions/main.js
+++ Functions/main.js
@@ -1,14 +1,32 @@
-// exercise 1
+// // exercise 1
+
+
+
+// const calcAge = function(currentYear,birthYear){
+
+// return currentYear-birthYear;
+
+
+
+// }
+
+// const age = calcAge(2017, 1989);
+// console.log(age);
+
+
+// exercise 2
 
 
 
 const calcAge = function(currentYear,birthYear){
-
-return currentYear-birthYear;
-
-
-
-}
-
-const age = calcAge(2017, 1989);
-console.log(age);
+    let age = currentYear-birthYear;
+    let result = `You are either ${age} or ${age-1}`
+    return result;
+    
+    
+    
+    }
+    
+    const age = calcAge(2018, 2015);
+    console.log(age);
+    
