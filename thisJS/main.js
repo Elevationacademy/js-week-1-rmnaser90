cc37b2f0576af858043827d2f55aa1feb2ab9f86
rmnaser90 <rmnaser90@gmail.com>
2020-10-29 11:15:53 +0300

    Exercise 2 done

--- thisJS/main.js
+++ thisJS/main.js
@@ -1,17 +1,42 @@
-//Excercise 1
+// //Excercise 1
 
-const person = {
-    hungry: true,
+// const person = {
+//     hungry: true,
   
-    feed: function () {
-      if (this.hungry) {
-        this.hungry = false;
-        alert('Im no longer hungry!')
-      }
+//     feed: function () {
+//       if (this.hungry) {
+//         this.hungry = false;
+//         alert('Im no longer hungry!')
+//       }
+//     }
+//   }
+  
+//   person.feed() //should alert "I'm no longer hungry"
+
+
+//   Excercise 2
+
+const pump = function (amount) {
+    this.liters += amount;
+    console.log('You put ' + amount + ' liters in ' + this.name);
+  };
+  
+  const garage = {
+    car1: {
+      name: 'Audi',
+      liters: 3,
+      fillTank: pump
+    },
+    car2: {
+      name: 'Mercedes',
+      liters: 1,
+      fillTank: pump
     }
-  }
+  };
   
-  person.feed() //should alert "I'm no longer hungry"
-
-
+  garage.car1.fillTank(2);
+  console.log('Audi should have 5 liters: ',  garage.car1.liters);
+  
+  garage.car2.fillTank(30);
+  console.log('Mercedes should have 31 liters: ', garage.car2.liters);
   
