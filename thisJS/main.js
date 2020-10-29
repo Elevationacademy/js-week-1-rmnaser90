6cba0c9bc192e029c18889cbf3fd1b55564798f7
rmnaser90 <rmnaser90@gmail.com>
2020-10-29 11:28:36 +0300

    exercise 3 done

--- thisJS/main.js
+++ thisJS/main.js
@@ -14,29 +14,57 @@
 //   person.feed() //should alert "I'm no longer hungry"
 
 
-//   Excercise 2
+// //   Excercise 2
 
-const pump = function (amount) {
-    this.liters += amount;
-    console.log('You put ' + amount + ' liters in ' + this.name);
+// const pump = function (amount) {
+//     this.liters += amount;
+//     console.log('You put ' + amount + ' liters in ' + this.name);
+//   };
+  
+//   const garage = {
+//     car1: {
+//       name: 'Audi',
+//       liters: 3,
+//       fillTank: pump
+//     },
+//     car2: {
+//       name: 'Mercedes',
+//       liters: 1,
+//       fillTank: pump
+//     }
+//   };
+  
+//   garage.car1.fillTank(2);
+//   console.log('Audi should have 5 liters: ',  garage.car1.liters);
+  
+//   garage.car2.fillTank(30);
+//   console.log('Mercedes should have 31 liters: ', garage.car2.liters);
+  
+
+
+// Exercise 3
+
+const pumpFuel = function (plane) {
+    plane.fuel += 1;
   };
   
-  const garage = {
-    car1: {
-      name: 'Audi',
-      liters: 3,
-      fillTank: pump
-    },
-    car2: {
-      name: 'Mercedes',
-      liters: 1,
-      fillTank: pump
+  const airplane = {
+      fuel:0,
+    fly: function () {
+      if (this.fuel < 2) {
+        return 'on the ground!';
+      }
+      else {
+        return 'flying!';
+      }
     }
   };
   
-  garage.car1.fillTank(2);
-  console.log('Audi should have 5 liters: ',  garage.car1.liters);
+  console.log('The plane should not be able to fly (yet): ' + airplane.fly());
   
-  garage.car2.fillTank(30);
-  console.log('Mercedes should have 31 liters: ', garage.car2.liters);
+  pumpFuel(airplane);
+  console.log('The plane should STILL not be able to fly: ' + airplane.fly());
+  
+  pumpFuel(airplane);
+  console.log('Take off! ' + airplane.fly());
   
